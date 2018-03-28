9605c120654699d05ff3d227f3e32a84dae0970b
Ricky Lung <dard12@gmail.com>
2018-03-27 22:57:56 -0400

    display suggested book

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,10 @@ class App extends Component {
       .get('/api/books')
       .then(res => {
         const books = res.data;
-        console.log(books);
         const book = _.first(books);
 
         if (book) {
-          this.setState({ book: book.title });
+          this.setState({ book: `"${book.title_suggest}"` });
         }
       })
       .catch();

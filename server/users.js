5a42026d76a5e2abd7a8874364aa59c8c5dd7941
Ricky Lung <dard12@gmail.com>
2018-04-14 22:32:26 -0400

    no name necessary

--- server/users.js
+++ server/users.js
@@ -33,9 +33,7 @@ usersSchema.pre('save', function(next) {
 const Users = mongoose.model('Users', usersSchema);
 
 passport.use(
-  'local',
   new LocalStrategy(function(username, password, done) {
-    console.log('CMONNNN');
     Users.findOne({ username })
       .then(user => {
         if (!user) {
@@ -76,33 +74,27 @@ passport.use(
 );
 
 app.post('/login', (req, res, next) => {
-  passport.authenticate(
-    'local',
-    {
-      session: false,
-    },
-    (err, user, info) => {
-      if (err || !user) {
-        console.log(err);
-        console.log(user);
-        console.log(info);
-        return res.status(400).json({
-          message: 'Something is not right',
-          user,
-        });
-      }
-
-      req.login(user, { session: false }, err => {
-        if (err) {
-          res.send(err);
-        }
-
-        const token = jwt.sign(user, 'your_jwt_secret');
-
-        return res.json({ user, token });
+  passport.authenticate('local', { session: false }, (err, user, info) => {
+    if (err || !user) {
+      console.log(err);
+      console.log(user);
+      console.log(info);
+      return res.status(400).json({
+        message: 'Something is not right',
+        user,
       });
     }
-  )(req, res);
+
+    req.login(user, { session: false }, err => {
+      if (err) {
+        res.send(err);
+      }
+
+      const token = jwt.sign(user, 'your_jwt_secret');
+
+      return res.json({ user, token });
+    });
+  })(req, res);
 });
 
 app.get('/logout', (req, res) => {

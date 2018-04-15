a198c6ab0db8833f727de21d54f7af85001430db
Ricky Lung <dard12@gmail.com>
2018-04-14 22:31:23 -0400

    changed to single quotes

--- server/mongoose.js
+++ server/mongoose.js
@@ -1,21 +1,21 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost/bookclub");
+mongoose.connect('mongodb://localhost/bookclub');
 
 function upsertTimestamps(schema) {
   schema.add({
     createdAt: Date,
-    updatedAt: Date
+    updatedAt: Date,
   });
 
-  schema.pre("save", function(next) {
+  schema.pre('save', function(next) {
     if (this.isNew) {
       this.createdAt = new Date();
     }
     next();
   });
 
-  schema.pre("update", function(next) {
+  schema.pre('update', function(next) {
     this.updatedAt = new Date();
     next();
   });

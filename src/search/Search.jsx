00672c600f2e98714b1503cba76d0e92d81c45a2
Ricky Lung <dard12@gmail.com>
2018-05-29 02:43:31 -0400

    mock search

--- src/search/Search.jsx
+++ src/search/Search.jsx
@@ -8,7 +8,7 @@ import './Search.css';
 class Search extends Component {
   constructor(props) {
     super(props);
-    this.state = { books: [] };
+    this.state = { books: [], isLoaded: false };
   }
 
   componentDidMount() {
@@ -16,24 +16,39 @@ class Search extends Component {
       .get('/user/profile')
       .then(res => {
         const user = res.data;
+
+        axios
+          .get('/api/books', { params: { bookIds: user.bookIds } })
+          .then(res => {
+            this.setState({ books: res.data, isLoaded: true });
+          })
+          .catch();
       })
       .catch();
   }
 
   render() {
     return (
-      <ul className="book-list">
-        {this.state.books.map((book, index) => (
-          <Link
-            to={'/journal/' + book._id}
-            key={index}
-            className="book-container"
-          >
-            <div className="book-title">{book.title}</div>
-            <div className="book-author">{_.first(book.author_name)}</div>
-          </Link>
-        ))}
-      </ul>
+      <div className="page-container">
+        <input
+          className="search-input input"
+          type="search"
+          placeholder="Search for a book..."
+        />
+
+        <ul className="book-list">
+          {this.state.books.map((book, index) => (
+            <Link
+              to={'/journal/' + book._id}
+              key={index}
+              className="book-container"
+            >
+              <div className="book-title">{book.title}</div>
+              <div className="book-author">{_.first(book.author_name)}</div>
+            </Link>
+          ))}
+        </ul>
+      </div>
     );
   }
 }

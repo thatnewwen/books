cf0a5e41197d1b35b9fa6bec383f21472c7a4590
Ricky Lung <dard12@gmail.com>
2018-07-12 20:56:52 -0400

    properly remove item

--- src/App.jsx
+++ src/App.jsx
@@ -42,8 +42,8 @@ class App extends Component {
   }
 
   logout() {
-    localStorage.setItem('token', null);
-    localStorage.setItem('user', null);
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
 
     axios = getAxios();
 

334dd060cbdb253ea59609e12f847c0b2c77e713
Ricky Lung <dard12@gmail.com>
2018-07-19 20:23:07 -0400

    add some button colors, toggle login/register

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -25,6 +25,17 @@ const loginForm = login => (
               submit: 'Username and password did not match any accounts.',
             });
           });
+      } else {
+        axios
+          .post('/register', values)
+          .then(res => setSubmitting(false))
+          .catch(err => {
+            setSubmitting(false);
+
+            setErrors({
+              submit: 'Username and password did not match any accounts.',
+            });
+          });
       }
     }}
     render={({ values, errors, handleChange, handleSubmit, isSubmitting }) => {
@@ -33,6 +44,31 @@ const loginForm = login => (
           history.push('/profile');
         });
       });
+
+      const pathEnd = getRoutePathEnd();
+      const isLogin = pathEnd === 'login';
+
+      let loginSwitch;
+
+      if (isLogin) {
+        loginSwitch = (
+          <div className="login-switch-container">
+            Don't have an account?
+            <a href="/register" class="link red">
+              Sign up →
+            </a>
+          </div>
+        );
+      } else {
+        loginSwitch = (
+          <div className="login-switch-container">
+            Have an account?
+            <a href="/login" class="link red">
+              Log in →
+            </a>
+          </div>
+        );
+      }
 
       return (
         <form onSubmit={handleSubmit} className="login-form">
@@ -46,6 +82,7 @@ const loginForm = login => (
               name="username"
               type="text"
               className="input"
+              placeholder="Your Email"
               value={values.email}
               onChange={handleChange}
             />
@@ -57,6 +94,7 @@ const loginForm = login => (
               name="password"
               type="password"
               className="input"
+              placeholder="Your Password"
               value={values.password}
               onChange={handleChange}
             />
@@ -65,11 +103,11 @@ const loginForm = login => (
           {errors.submit && <div className="login-error">{errors.submit}</div>}
 
           <button
-            className="btn password-auth"
+            className={'btn password-auth ' + (isLogin ? '' : 'blue')}
             type="submit"
             disabled={isSubmitting}
           >
-            {getRoutePathEnd() === 'login' ? 'Log In' : 'Sign Up'}
+            {isLogin ? 'Log In' : 'Sign Up'}
           </button>
 
           <a className="btn facebook-auth" href="/auth/facebook">
@@ -79,6 +117,8 @@ const loginForm = login => (
           <a className="btn google-auth" href="/auth/google">
             Continue with Google
           </a>
+
+          {loginSwitch}
         </form>
       );
     }}
